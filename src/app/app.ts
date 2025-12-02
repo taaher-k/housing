c8d8eaa8f07f6fdc4854ded1abd39cf54fefb919
taaher-k <taaher.rehmanii@gmail.com>
2025-12-02 22:52:12 +0530

    housing v.0.002

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  
 })
+
+
 export class App {
   protected readonly title = signal('housing');
 }
